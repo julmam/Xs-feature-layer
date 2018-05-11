0a6b6727b3f9128cdc7da746ee5b76fea7fa8e04
xserverinternet <oliver.heilig@ptvgroup.com>
2018-05-12 00:11:13 +0200

    Token update

--- token.js
+++ token.js
@@ -1,3 +1,3 @@
 // The token below is for internal use only - consider getting your own token!
-// This token is valid until 2018-07-27T16:07:42Z and was auto-generated @ 2018-05-10T21:16:47Z.
+// This token is valid until 2018-07-27T16:07:42Z and was auto-generated @ 2018-05-11T21:16:39Z.
 var token = '74BBCCA9-FE98-463D-AC9F-5C269E744284';
